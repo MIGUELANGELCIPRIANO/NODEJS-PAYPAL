8275a2fbdc3e6031c9b09640f6ad407d9df83313
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2024-02-06 09:39:13 -0300

    PERF: Create Order with Token

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -2,33 +2,59 @@ import axios from 'axios'
 import { PAYPAL_API, PAYPAL_API_CLIENT, PAYPAL_API_SECRET } from '../config'
 
 export const createOrder = async (req, res) => {
-	const order = {
-		intent: 'CAPTURE',
-		purchase_units: [
-			{
-				amount: {
-					currency_code: 'USD',
-					value: '20',
+	try {
+		const order = {
+			intent: 'CAPTURE',
+			purchase_units: [
+				{
+					amount: {
+						currency_code: 'USD',
+						value: '20',
+					},
+					description: 'Natsu Basho Tournament Ticket',
 				},
-				description: 'Natsu Basho Tournament Ticket',
+			],
+			application_context: {
+				brand_name: 'sumocompany',
+				landing_page: 'LOGIN',
+				user_action: 'PAY_NOW',
+				return_url: 'http://localhost:4000/capture-order',
+				cancel_url: 'http://localhost:4000/cancel-order',
 			},
-		],
-		application_context: {
-			brand_name: 'sumocompany',
-			landing_page: 'LOGIN',
-			user_action: 'PAY_NOW',
-			return_url: 'http://localhost:4000/capture-order',
-			cancel_url: 'http://localhost:4000/cancel-order',
-		},
+		}
+
+		const params = new URLSearchParams()
+		params.append('grant_type', 'client_credentials')
+
+		const {
+			data: { access_token },
+		} = await axios.post(
+			'https://api-m.sandbox.paypal.com/v1/oauth2/token',
+			params,
+			{
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+				},
+				auth: {
+					username: PAYPAL_API_CLIENT,
+					password: PAYPAL_API_SECRET,
+				},
+			}
+		)
+
+		const response = await axios.post(
+			`${PAYPAL_API}/v2/checkout/orders`,
+			order,
+			{
+				headers: {
+					Authorization: `Bearer ${access_token}`,
+				},
+			}
+		)
+		res.status(200).json(response.data)
+	} catch (error) {
+		res.status(500).json({ message: error.message })
 	}
-	const response = await axios.post(`${PAYPAL_API}/v2/checkout/orders`, order, {
-		auth: {
-			username: PAYPAL_API_CLIENT,
-			password: PAYPAL_API_SECRET,
-		},
-	})
-	console.log(response.data)
-	res.send('create order')
 }
 
 export const captureOrder = (req, res) => {

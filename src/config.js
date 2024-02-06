1adf6419036b0b5b4c12d8e4ac2c29c484037059
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2024-02-06 00:14:41 -0300

    ADD Paypal Credentials

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export const PAYPAL_API_CLIENT =
+	'AepcANuqWWuv5yA5uSHY4Yxfz7f9M2W-tXb-H4UxS1dLzCHfyS3zYR0_QXwKtalR-sXbiBTwEWff0n7A'
+export const PAYPAL_API_SECRET =
+	'EKPzmqUy6Z8m6hYnro61Wre-WCAC8FmDnkuoglaMR9or1lZx8SwebPxZwIS7At5MJVqtx2xvGCe7xTEQ'
+export const PAYPAL_API = 'https://api-m.paypal.com'
+export const PORT = 3000
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import paymentRoutes from './routes/payment.routes'
 import morgan from 'morgan'
+import { PORT } from './config'
 
 const app = express()
 
 app.use(morgan('dev'))
 app.use(paymentRoutes)
 
-app.listen(3000)
-console.log('Server listening on port', 3000)
+app.listen(PORT)
+console.log('Server listening on port', PORT)

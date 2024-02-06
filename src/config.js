2df49c81434eb892ff89d4117b2ebf332b509eca
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2024-02-06 17:27:08 -0300

    PERF: Environment Variables

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
-export const PAYPAL_API_CLIENT =
-	'AepcANuqWWuv5yA5uSHY4Yxfz7f9M2W-tXb-H4UxS1dLzCHfyS3zYR0_QXwKtalR-sXbiBTwEWff0n7A'
-export const PAYPAL_API_SECRET =
-	'EKPzmqUy6Z8m6hYnro61Wre-WCAC8FmDnkuoglaMR9or1lZx8SwebPxZwIS7At5MJVqtx2xvGCe7xTEQ'
-export const PAYPAL_API = 'https://api-m.sandbox.paypal.com'
-export const HOST = 'http://localhost:3000'
-export const PORT = 3000
+import { config } from 'dotenv'
+
+config()
+
+export const PAYPAL_API = process.env.PAYPAL_API
+export const PAYPAL_API_CLIENT = process.env.PAYPAL_API_CLIENT
+export const PAYPAL_API_SECRET = process.env.PAYPAL_API_SECRET
+export const HOST = process.env.HOST
+export const PORT = process.env.PORT
